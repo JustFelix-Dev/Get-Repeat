a3ac3853e22e879b52cea1fdb925312b37af5179
Felix Owolabi <owolabifelix78@gmail.com>
2024-03-07 11:16:37 +0100

    feat: completed hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+
+const Hero = () => {
+  return (
+    <section className='min-h-[492px] flex items-end bg-custom-yellow'>
+      <div className="hero flex flex-col gap-4 w-full max-w-[990px] mx-auto pb-[45px] ">
+        <header  className="font-semibold text-based text-[48.875px] leading-[63.5375px] font-poppins max-w-[20ch]" >SUPERCHARGED LIFECYCLE MARKETING</header>
+        <p className="text-based font-montserrat font-17 max-w-[60ch]" ><span className="font-semibold" >Repeat boosts repurchase rate</span> by automatically delivering personalized messaging at high-leverage Moments<sup className="font-semibold">TM</sup>.
+        </p>
+        <div className="flex flex-col gap-4 w-full max-w-[665px]">
+            <button className=" text-left font-montserrat text-based font-semibold text-[34px] py-[17px] px-[34px] bg-white border border-based rounded-full w-full max-w-[665px]"><span>→
+                </span> GET STARTED</button>
+                <a href="#" className=" font-montserrat underline self-center text-based text-17">Or , book a demo</a>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
