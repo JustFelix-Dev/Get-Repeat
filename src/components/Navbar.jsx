a765fd05186ee93292ef83c6588531afb0106d9f
Felix Owolabi <owolabifelix78@gmail.com>
2024-03-07 09:32:30 +0100

    initial commit 🎉🎉

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+const Navbar = () => {
+  return (
+    <nav className="bg-red-800">
+      <div className="logo"></div>  
+      <ul>
+        <li><a href="#"><span>Product</span></a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
