14e5ba5ef3b4b46d1ab77091a6eb9d6a9a4ccfe4
Felix Owolabi <owolabifelix78@gmail.com>
2024-03-08 13:25:50 +0100

    feat: adjusted page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
     <div className="logo cursor-pointer">
       <img src={navLogo} alt="nav-logo" width={'90px'} height={'44px'} />
     </div>  
-      <ul className='flex items-center'>
-        <li className='product relative py-[14px] px-[20px]'><a href="#" className='flex items-center text-based font-montserrat font-medium opacity-[0.7] text-17'><span>Product</span><svg height={'100px'}  viewBox="0 0 24 24" fill="#171717" className=' w-[20px]'>
+      <ul className='flex items-center justify-center w-full max-w-[644.33px]'>
+        <li className='product relative py-[14px] px-[20px]'><a href="#" className='flex items-center text-based font-montserrat font-medium opacity-[0.7] hover:opacity-100 transition-all text-17'><span>Product</span><svg height={'100px'}  viewBox="0 0 24 24" fill="#171717" className=' w-[20px]'>
         <path d="M17 11l-5 5-5-5z"/>
         </svg>
         </a>
@@ -19,10 +19,11 @@ const Navbar = () => {
             <li className='transition-all py-[1rem] px-[0.8rem] hover:bg-custom-yellow rounded-lg'><a href="#">Pricing</a></li>
          </ul>
         </li>
-        <li className='py-[14px] px-[20px]' ><a href="#" className='text-based font-montserrat font-medium opacity-[0.7] text-17'>Blog</a></li>
-        <li className='py-[14px] px-[20px]' ><a href="#" className='text-based font-montserrat font-medium opacity-[0.7] text-17'>Stop Focusing on LTV</a></li>
+        <li className='py-[14px] px-[20px]' ><a href="#" className='text-based font-montserrat font-medium opacity-[0.7]  hover:opacity-100 transition-all text-17'>Blog</a></li>
+        <li className='py-[14px] px-[20px]' ><a href="#" className='text-based font-montserrat font-medium opacity-[0.7]  hover:opacity-100 transition-all text-17'>Stop Focusing on LTV</a></li>
       </ul>
-      <button className='bg-[#37352F] text-white py-[6px] px-[16px] text-17 font-montserrat rounded-full'>GET STARTED</button>
+      <button className='bg-[#37352F] hover:bg-[rgb(55,53,47,0.8)] transition-all 
+    text-white py-[6px] px-[16px] text-17 font-montserrat rounded-full'>GET STARTED</button>
     </nav>
   )
 }
